e28e34d91fd4b577779a9dd57cc29c3d6e569b1f
Jason Chen <jason.sl.chen@gmail.com>
2019-10-23 18:33:28 -0700

    solution for maxSubArray

--- maxSubArray.js
+++ maxSubArray.js
@@ -5,3 +5,14 @@
 // Input: [-2,1,-3,4,-1,2,1,-5,4],
 // Output: 6
 // Explanation: [4,-1,2,1] has the largest sum = 6.
+
+var maxSubArray = function(nums) {
+  let maxSum = nums[0];
+  let currentSum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+      
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    maxSum = Math.max(currentSum, maxSum);
+  }
+  return maxSum;
+};
