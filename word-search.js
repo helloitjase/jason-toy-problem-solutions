d7ba855c4777b45cf14c67a824166ae0f29b3150
Jason Chen <jason.sl.chen@gmail.com>
2019-10-26 09:59:10 -0700

    complete so lution, needed help

--- word-search.js
+++ word-search.js
@@ -1,44 +1,36 @@
 var exist = function(board, word) {
   let wordExist = false;
   // debugger;
-  const wordFinder = (x, y, index, builtWord, boardVersion) => {
-    if (builtWord.length === word.length && builtWord !== word) {
-      return;
-    }
-    
-    if (wordExist === true) {
-      return;
-    }
-
-    if (board[x][y] === word[index]) {
-      boardVersion[x][y] = true;
-      builtWord += word[index];
-      index++;
-      if (builtWord === word) {
+  const wordFinder = (x, y, index) => {
+    if (!wordExist) {
+      if (x < 0 || y < 0 || x > board.length - 1 || y > board[0].length - 1) {
+        return;
+      }
+      if (board[x][y] !== word[index]) {
+        return;
+      }
+      if (index === word.length - 1) {
         wordExist = true;
         return;
       }
-    
-      if (board[x + 1] !== undefined && wordExist === false) {
-        wordFinder(x + 1, y, index, builtWord, boardVersion);
-      }    
-      if (board[x - 1] !== undefined && wordExist === false) {
-        wordFinder(x - 1, y, index, builtWord, boardVersion);
-      }
-      if (board[x][y + 1] !== undefined && wordExist === false) {
-        wordFinder(x, y + 1, index, builtWord, boardVersion);
-      }
-      if (board[x][y - 1] !== undefined && wordExist === false) {
-        wordFinder(x, y - 1, index, builtWord, boardVersion);
-      }
+      board[x][y] = null;
+      wordFinder(x + 1, y, index + 1);
+      wordFinder(x - 1, y, index + 1);
+      wordFinder(x, y + 1, index + 1);
+      wordFinder(x, y - 1, index + 1);
+      board[x][y] = word[index];
+
     }
   }
 
 
   for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board[i].length; j++) {
-        if (board[i][j] === word[0] && !wordExist) {
-          wordFinder(i, j, 0, '', board);
+        if (board[i][j] === word[0]) {
+          wordFinder(i, j, 0);
+          if (wordExist) {
+            return wordExist;
+          }
         }
       }
   }    
