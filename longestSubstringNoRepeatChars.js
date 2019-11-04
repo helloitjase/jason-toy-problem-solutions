807c69cf1025df85db7259078f1a54c42ce51c28
Jason Chen <jason.sl.chen@gmail.com>
2019-11-04 11:14:19 -0800

    more optimized solution for longestSubstringNoRepeatChars, sliding window solution

--- longestSubstringNoRepeatChars.js
+++ longestSubstringNoRepeatChars.js
@@ -1,24 +1,75 @@
 var lengthOfLongestSubstring = function(s) {
-    let counts = [];
-    if (s === '') {
-        return 0;
+  let i = 0;
+  let j = 0;
+  let ans = 0;
+  let set = {};
+  while (i < s.length && j < s.length) {
+    if (!set[s[j]]) {
+      set[s[j]] = s[j];
+      j++;
+    } else if (set[s[j]]) {
+      delete set[s[i]];
+      i++;
     }
-    if (s.length < 2) {
-        return 1;
-    }
-    for (let i = 0; i < s.length; i++) {
-      let counter = 1;
-      let substr = s[i];
-      
-      for (let j = i + 1; j < s.length; j++) {
-          if (!substr.includes(s[j])) {
-              counter++;
-              substr += s[j];
-          } else {
-              break;
-          }
-      }
-      counts.push(counter);  
+    ans = Math.max(ans, j - i);
   }
-    return Math.max(...counts);
+  return ans;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var lengthOfLongestSubstring = function(s) {
+
+//     let counts = [];
+//     if (s === '') {
+//         return 0;
+//     }
+//     if (s.length < 2) {
+//         return 1;
+//     }
+//     for (let i = 0; i < s.length; i++) {
+//       let counter = 1;
+//       let substr = s[i];
+      
+//       for (let j = i + 1; j < s.length; j++) {
+//           if (!substr.includes(s[j])) {
+//               counter++;
+//               substr += s[j];
+//           } else {
+//               break;
+//           }
+//       }
+//       counts.push(counter);  
+//   }
+//     return Math.max(...counts);
+// }
